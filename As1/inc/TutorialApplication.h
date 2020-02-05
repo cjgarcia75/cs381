54829ca5da418b56e4d9a8fef57fd4c946c97b75
cjgarcia75 <44075677+cjgarcia75@users.noreply.github.com>
2020-02-05 12:44:33 -0800

    Add files via upload

---
+++ As1/inc/TutorialApplication.h
@@ -0,0 +1,39 @@
+/*
+ * Christian Garcia
+ * cj_garcia@sbcglobal.net
+-----------------------------------------------------------------------------
+Filename:    TutorialApplication.h
+-----------------------------------------------------------------------------
+
+This source file is part of the
+   ___                 __    __ _ _    _
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/
+	  Tutorial Framework
+	  http://www.ogre3d.org/tikiwiki/
+-----------------------------------------------------------------------------
+*/
+#ifndef __TutorialApplication_h_
+#define __TutorialApplication_h_
+
+#include "BaseApplication.h"
+
+class TutorialApplication : public BaseApplication
+{
+public:
+	TutorialApplication(void);
+	virtual ~TutorialApplication(void);
+
+protected:
+	virtual void createScene(void);
+
+	Ogre::SceneNode* ogreNode;
+	Ogre::SceneNode* ogreNode2;
+	Ogre::SceneNode* ogreNode3;
+	Ogre::SceneNode* ogreNode4;
+};
+
+#endif // #ifndef __TutorialApplication_h_
